281e8a0df0ae8ca31f67ae8bc23123c8a1fcef02
alam0055 <alam0055@student.monash.edu>
2026-01-28 19:18:46 +0800

    cat

--- bluesclues/src/components/statuscard.tsx
+++ bluesclues/src/components/statuscard.tsx
@@ -1,15 +1,18 @@
 export default function StatusCard() {
   return (
-    <div className="border rounded-lg border-zinc-800 bg-zinc-200 p-8">
-      <p className="var-h text-lg font-semibold text-zinc-900 text-center mb-6">
-        Currently Working On:
-      </p>
-      <ul>
-        <li>Internship: Theodesis Sdn Bhd.</li>
-        <li>Contract: Pemborong Maju Bersatu Sdn Bhd.</li>
-        <li>Hackathon: Chin Hin AI Hackathon 2026</li>
-        <li></li>
-      </ul>
+    <div className=" rounded-lg border-zinc-800 bg-zinc-200 p-8 flex items-center gap-16">
+      <div>
+        <p className="var-h text-lg font-semibold text-zinc-900 text-center mb-6">
+          Currently Working On:
+        </p>
+        <ul>
+          <li>Internship: Theodesis Sdn Bhd.</li>
+          <li>Contract: Pemborong Maju Bersatu Sdn Bhd.</li>
+          <li>Hackathon: Chin Hin AI Hackathon 2026</li>
+          <li></li>
+        </ul>
+      </div>
+      <img src="/cat.jpg" alt="notmycat" className="w-28 h-28 rounded-lg" />
     </div>
   );
 }
