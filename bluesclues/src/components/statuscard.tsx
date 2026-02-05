ead7d1e2f8d68c4c724b7966a815e935576dba7a
alam0055 <alam0055@student.monash.edu>
2026-02-05 22:33:17 +0800

    card lg:justify-between

--- bluesclues/src/components/statuscard.tsx
+++ bluesclues/src/components/statuscard.tsx
@@ -15,30 +15,23 @@ export default function StatusCard() {
     >
 
       <div
-        className="transition-transform duration-2000 shadow-lg"
+        className="transition-transform duration-1000 shadow-lg"
         style={{
           transformStyle: "preserve-3d", // lets children exist in 3D space
           transform: flipped ? "rotateX(180deg)" : "rotateX(0deg)",
         }}
       >
 
-        <div
-          className=" rounded-lg bg-zinc-200 p-8 flex flex-col lg:flex-row items-center gap-8"
-          style={{ backfaceVisibility: "hidden" }} // hide opposite side (if one side is smaller than the other)
+        <div // force larger layout
+          className="invisible p-8 flex flex-col lg:flex-row items-center gap-2 lg:justify-between"
         >
           <div>
-            <p className="var-h text-lg font-semibold text-zinc-900 text-center">
-              Currently Working On:
+            <p className="text-lg font-semibold text-zinc-900 text-center">
+              Increase card with here h
             </p>
             <p className="text-xs text-zinc-400 text-center mb-4">
               As of {dayjs().format("DD-MM-YYYY")}
             </p>
-            <ul>
-              <li>Internship: Theodesis Sdn Bhd.</li>
-              <li>Contract: Pemborong Maju Bersatu Sdn Bhd.</li>
-              <li>Hackathon: Chin Hin AI Hackathon 2026</li>
-              <li></li>
-            </ul>
           </div>
           <Image
             src="/cat.jpg"
@@ -49,23 +42,17 @@ export default function StatusCard() {
           />
         </div>
 
-        <div // alligned at perfectly
-          className="absolute inset-0  rounded-lg bg-zinc-200 p-8 flex flex-col lg:flex-row items-center gap-8"
-          style={{ backfaceVisibility: "hidden", transform: "rotateX(180deg)" }}
+        <div // front (allgined perfectly)
+          className="absolute inset-0 rounded-lg bg-zinc-200 p-8 flex flex-col lg:flex-row items-center lg:justify-between"
+          style={{ backfaceVisibility: "hidden" }} // hide opposite side (if one side is smaller than the other)
         >
           <div>
-            <p className="var-h text-lg font-semibold text-zinc-900 text-center">
+            <p className="text-lg font-semibold text-zinc-900 text-center">
               Currently Working On:
             </p>
             <p className="text-xs text-zinc-400 text-center mb-4">
               As of {dayjs().format("DD-MM-YYYY")}
             </p>
-            <ul>
-              <li>Internship: Theodesis Sdn Bhd.</li>
-              <li>Contract: Pemborong Maju Bersatu Sdn Bhd.</li>
-              <li>Hackathon: Chin Hin AI Hackathon 2026</li>
-              <li></li>
-            </ul>
           </div>
           <Image
             src="/cat.jpg"
@@ -74,6 +61,29 @@ export default function StatusCard() {
             height={100}
             className="rounded-lg"
           />
+        </div>
+
+        <div // back
+          className="absolute inset-0 rounded-lg bg-zinc-200 p-8 flex flex-col lg:flex-row items-center"
+          style={{ backfaceVisibility: "hidden", transform: "rotateX(180deg)" }}
+        >
+          <div>
+            <p className="text-xs text-zinc-400 text-center mb-4">
+              As of {dayjs().format("DD-MM-YYYY")}
+            </p>
+            <ul>
+              <li>
+                <b>Internship:</b> Theodesis Sdn Bhd.
+              </li>
+              <li>
+                <b>Contract:</b> Pemborong Maju Bersatu Sdn Bhd.
+              </li>
+              <li>
+                <b>Hackathon:</b> Chin Hin AI Hackathon 2026
+              </li>
+              <li></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
