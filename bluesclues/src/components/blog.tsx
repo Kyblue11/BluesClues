846dd8b6408e5926299a5ef5eaed03c28f038fe8
alam0055 <alam0055@student.monash.edu>
2026-02-05 22:46:56 +0800

    pt 2

--- bluesclues/src/components/blog.tsx
+++ bluesclues/src/components/blog.tsx
@@ -7,7 +7,7 @@ export default function MyBlog({ blogs }: { blogs: Blog[] }) {
       {blogs.map((blog, _) => (
         <li key={blog.blog_slug} className="relative">
           <Link href={`/blog/${blog.blog_slug}`}>
-            <div className="flex justify-between items-start mb-1">
+            <div className="flex flex-col lg:flex-row justify-between items-start mb-1">
               <span className="var-h2 underline">{blog.blog_title}</span>
               <span className="text-sm text-zinc-500 font-normal">
                 {blog.blog_date}
